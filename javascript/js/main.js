1b19a946482d3dafc44ba91b157e4bad4ace0098
AravinthanSathasivam <aravinthan0015@gmail.com>
2023-05-05 15:35:34 +0200

    Second commit

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -1,35 +1,41 @@
-console.log('Hello world from js file');
+console.log('Hello from js file');
 
 let cellsEl = document.querySelectorAll('.cell');
 
-let playerStick = ['X', 'O']
+let playerStick = ['X', 'O'];
+let playersName = ['Player 1', 'Player 2'];
+let playersScore = [0,0];
 let currentPlayer = 0;
 let msgEl = document.querySelector('#msg');
 let end = false;
+
+//Show default player names
+let namePlayer1El = document.querySelector("#player1 .name");
+let namePlayer2El = document.querySelector("#player2 .name");
+namePlayer1El.innerHTML = playersName[0];
+namePlayer2El.innerHTML = playersName[1];
+
+//Show default player scores
+let scorePlayer1El = document.querySelector("#player1 .score");
+let scorePlayer2El = document.querySelector("#player2 .score");
+scorePlayer1El.innerHTML = playersScore[0];
+scorePlayer2El.innerHTML = playersScore[1];
+
 
 function showMsg(msg){
     msgEl.innerHTML = msg;
     msgEl.style.display = 'block';
 }
 
+
+// hide the message when its finished
 const hideMsg = () => {
     msgEl.innerHTML = '';
     msgEl.style.display = 'none';
 }
 
 
-const clearAll = () =>{
-    cellsEl[0].innerHTML == ' ';
-    cellsEl[1].innerHTML == ' ';
-    cellsEl[2].innerHTML == ' ';
-    cellsEl[3].innerHTML == ' ';
-    cellsEl[4].innerHTML == ' ';
-    cellsEl[5].innerHTML == ' ';
-    cellsEl[6].innerHTML == ' ';
-    cellsEl[7].innerHTML == ' ';
-    cellsEl[8].innerHTML == ' ';
-
-}
+//
 
 const verify = () => {
 
@@ -38,32 +44,40 @@ const verify = () => {
         cellsEl[1].innerHTML == playerStick[currentPlayer]&&
         cellsEl[2].innerHTML == playerStick[currentPlayer]
     ){
-        alert(currentPlayer + " win");
-        clearAll();
+        cellsEl[0].style.background = '#2ecc71';
+        cellsEl[1].style.background = '#2ecc71';
+        cellsEl[2].style.background = '#2ecc71';
+        showMsg('Game Ended!!' + currentPlayer + " win");
 
     }else if(
         cellsEl[3].innerHTML == playerStick[currentPlayer]&&
         cellsEl[4].innerHTML == playerStick[currentPlayer]&&
         cellsEl[5].innerHTML == playerStick[currentPlayer]
     ){
-        alert(currentPlayer + " win");
-        clearAll();
+        cellsEl[3].style.background = '#2ecc71';
+        cellsEl[4].style.background = '#2ecc71';
+        cellsEl[5].style.background = '#2ecc71';
+        showMsg('Game Ended!!' + currentPlayer + " win");
 
     }else if(
         cellsEl[6].innerHTML == playerStick[currentPlayer]&&
         cellsEl[7].innerHTML == playerStick[currentPlayer]&&
         cellsEl[8].innerHTML == playerStick[currentPlayer]
     ){
-        alert(currentPlayer + " win");
-        clearAll();
+        cellsEl[6].style.background = '#2ecc71';
+        cellsEl[7].style.background = '#2ecc71';
+        cellsEl[8].style.background = '#2ecc71';
+        showMsg('Game Ended!!' + currentPlayer + " win");
     }
     else if (
         cellsEl[0].innerHTML == playerStick[currentPlayer]&&
         cellsEl[4].innerHTML == playerStick[currentPlayer]&&
         cellsEl[8].innerHTML == playerStick[currentPlayer]
     ){
-        alert(currentPlayer + " win");
-        clearAll();
+        cellsEl[0].style.background = '#2ecc71';
+        cellsEl[4].style.background = '#2ecc71';
+        cellsEl[8].style.background = '#2ecc71';
+        showMsg('Game Ended!!' + currentPlayer + " win");
 
     }else if (
         cellsEl[2].innerHTML == playerStick[currentPlayer]&&
@@ -71,17 +85,40 @@ const verify = () => {
         cellsEl[6].innerHTML == playerStick[currentPlayer]
 
     ){
-        alert(currentPlayer + " win");
-        clearAll();
+        cellsEl[2].style.background = '#2ecc71';
+        cellsEl[4].style.background = '#2ecc71';
+        cellsEl[6].style.background = '#2ecc71';
+        showMsg('Game Ended!!' + currentPlayer + " win");
 
     }else if(
         cellsEl[0].innerHTML == playerStick[currentPlayer]&&
         cellsEl[3].innerHTML == playerStick[currentPlayer]&&
         cellsEl[6].innerHTML == playerStick[currentPlayer]
     ){
-        alert(currentPlayer + " win");
-        clearAll();
+        cellsEl[0].style.background = '#2ecc71';
+        cellsEl[3].style.background = '#2ecc71';
+        cellsEl[6].style.background = '#2ecc71';
+        showMsg('Game Ended!!' + currentPlayer + " win");
 
+    }else if(
+        cellsEl[1].innerHTML == playerStick[currentPlayer]&&
+        cellsEl[4].innerHTML == playerStick[currentPlayer]&&
+        cellsEl[7].innerHTML == playerStick[currentPlayer]
+    ){
+        cellsEl[01].style.background = '#2ecc71';
+        cellsEl[4].style.background = '#2ecc71';
+        cellsEl[7].style.background = '#2ecc71';
+        showMsg('Game Ended!!' + currentPlayer + " win");
+    }
+    else if(
+        cellsEl[2].innerHTML == playerStick[currentPlayer]&&
+        cellsEl[5].innerHTML == playerStick[currentPlayer]&&
+        cellsEl[8].innerHTML == playerStick[currentPlayer]
+    ){
+        cellsEl[2].style.background = '#2ecc71';
+        cellsEl[5].style.background = '#2ecc71';
+        cellsEl[8].style.background = '#2ecc71';
+        showMsg('Game Ended!!' + currentPlayer + " win");
     }
     
     cellsEl.forEach(cellsEl => {
